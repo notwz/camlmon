5bc99a9ecb16b293e79992aee71a3be74bd1a917
Will Zhang <wz282@cornell.edu>
2022-03-04 20:48:23 -0500

    Tenative completed Pokemon state

--- src/p_state.ml
+++ src/p_state.ml
@@ -1,26 +1,59 @@
 type moves_left = { move_name : string; current_count: int; max_count: int }
 type t = { hp : int; moves : moves_left list }
 
-exception NoMove
+exception InvalidMove
 let rec find_move (moves) ( move: string) = 
   match moves with 
   | h :: t -> if h.move_name = move then 
     h else find_move t move 
-  | [] -> raise NoMove
+  | [] -> raise InvalidMove
   
 
 let valid_move (state : t) (move : string) : bool = 
   let move_info = find_move state.moves move in 
+  if move_info = raise InvalidMove then false else
   let moves_left = move_info.current_count in 
-  if (moves_left < 0) then true else false 
+  if ( moves_left < 0 ) then true else false 
+
+
+let rec update_moves_list (m_list: moves_left list) (move: string) : moves_left list = 
+ match m_list with 
+  | [h] -> if h.move_name = move then [{h with current_count = h.current_count -1}] else 
+    [h] 
+  | h::t -> if h.move_name = move then {h with current_count = h.current_count -1 } :: t else 
+  update_moves_list t move 
+  | [] -> [] 
+  
+let use_move (state : t) (move: string) : t = 
+  let new_moves_set = update_moves_list state.moves move in 
+  { hp = state.hp; moves = new_moves_set}
+
+let damaged (state : t) (damage : int) : t = 
+  { state with hp =  state.hp - damage }
+
 
 let test_moves : moves_left list = 
-  [{move_name = "cut"; current_count = 2; max_count = 5};
-  {move_name = "fly"; current_count = 2; max_count = 5};
-  {move_name = "thunder"; current_count = 2; max_count = 5};
-  {move_name = "shove"; current_count = 0; max_count = 5}]
+  [
+    {move_name = "cut"; current_count = 2; max_count = 5};
+    {move_name = "fly"; current_count = 2; max_count = 5};
+    {move_name = "thunder"; current_count = 2; max_count = 5};
+    {move_name = "shove"; current_count = 0; max_count = 5}
+  ]
+
+let new_test_moves : moves_left list = 
+  [
+    {move_name = "cut"; current_count = 1; max_count = 5};
+    {move_name = "fly"; current_count = 2; max_count = 5};
+    {move_name = "thunder"; current_count = 2; max_count = 5};
+    {move_name = "shove"; current_count = 0; max_count = 5}
+  ]
+
+  
 let test_p_state : t =  
-  { hp = 12; moves = test_moves}
+  { hp = 12; moves = test_moves }
+
+let new_test_p_state : t = 
+  { hp = 12; moves = new_test_moves }
 
   
 
--- src/p_state.mli
+++ src/p_state.mli
@@ -1,6 +1,22 @@
 type moves_left
 type t 
 
+exception InvalidMove
+
+(** [valid_move] returns true or false if the move is in 
+  the pokemon's current move set and if the current count
+    of moves left is > 0  *)
 val valid_move : t -> string -> bool 
 
+(** [use_move] returns the user's current pokemon's 
+state after it uses a move. It should decrement the 
+current count of moves left for the move it just used  *)
+val use_move : t -> string -> t
+
+(** [damaged] returns a new pokemon state after it 
+has been targeted by an attack move from opponent pokemon  *)
+val damaged : t -> int -> t 
+
+(** [test_p_state] are for testing purposes   *)
 val test_p_state : t 
+val new_test_p_state : t

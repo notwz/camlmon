4d2630e3bea8f22e3835f78e2332d21854ce8e7e
Will Zhang <wz282@cornell.edu>
2022-03-12 20:04:01 -0500

    SDL library added, bin/main file beginner code for main menu
    - opam ocamlsdl2

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,20 @@
-open Lib 
+open Lib
+open Game
+open Sdl
 
-let () = 
-let result = Math.add 2 3 in 
-print_endline ( string_of_int result); 
-let result = Math.sub 3 1 in 
-print_endline (string_of_int result);
-print_endline "hello world! "
+let rec listen (input : string) =
+  print_endline ("your input was " ^ input)
+
+let main () =
+  ANSITerminal.print_string [ ANSITerminal.green ]
+    "\n\n Welcome to the Camlmon Pokemon Game written in OCaml! \n";
+  print_endline " Main Menu ";
+  print_endline " >>> [ 1 ]  catch pokemon";
+  print_endline " >>> [ 2 ]  battle pokemon";
+  print_endline " >>> [ 3 ]  battle users";
+  print_endline " type in ' 1'  '2' or '3'\n to choose option to play! ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | x -> listen x
+
+let () = main ()

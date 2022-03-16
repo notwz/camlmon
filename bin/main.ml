c9c358cb733689fa038dcca311f7177c9d1a0d8a
Will Zhang <wz282@cornell.edu>
2022-03-16 02:52:26 -0400

    Refined the map explore function for more understandability

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,17 @@
-(*open OCLib*)
 open Game
 open Sdl
 open Lib
 open Test
 open Printf
+open Gui
+open Explore
 
 let rec listen (input : string) =
   print_endline ("You choose: " ^ input);
   match input with
-  | "1" -> test_main ()
-  | "2" -> test_main ()
-  | "3" -> test_main ()
+  | "1" -> explore_main "Ash Ketchum" 1
+  | "2" -> explore_main "Ash Ketchum" 1
+  | "3" -> explore_main "Ash Ketchum" 1
   | _ -> (
       print_endline "please put a valid input";
       match read_line () with
@@ -21,8 +22,8 @@ let main () =
     "\n\n Welcome to the Camlmon Pokemon Game written in OCaml! \n";
   print_endline " Main Menu ";
   print_endline " >>> [ 1 ]  catch pokemon";
-  print_endline " >>> [ 2 ]  battle pokemon";
-  print_endline " >>> [ 3 ]  battle users";
+  print_endline " >>> [ 2 ]  battle pokemon [not implemented]";
+  print_endline " >>> [ 3 ]  battle users [not implemented]";
   print_endline " type in ' 1'  '2' or '3'\n to choose option to play! ";
   match read_line () with
   | exception End_of_file -> ()
--- src/explore.ml
+++ src/explore.ml
@@ -22,12 +22,18 @@ let rec take_turn map =
       let new_map = Map.move_left map in
       let () = print_endline (Map.pp_map new_map) in
       take_turn new_map
-  | _ ->
+  | "q" ->
       let () = print_endline "Quitting" in
       ()
+  | _ ->
+      print_endline "Please put a valid command:";
+      take_turn map
 
 let explore_main name id =
   let trainer = Trainer.init_trainer name id in
   let trainer_state = T_state.init_t_state trainer in
   let map = Map.init_map trainer_state in
+  print_endline
+    "Type 'up', 'down', 'left', 'right' to move around in the map. \
+     ('q' to quit.) ";
   take_turn map

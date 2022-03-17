a51ee8ade70cb85007455b434aedbc86077d4c3b
Will Zhang <wz282@cornell.edu>
2022-03-16 21:36:31 -0400

    Finished Main Menu and Interface for MS1!!

--- src/test.ml
+++ src/test.ml
@@ -40,13 +40,13 @@ let rec main_menu_terminal =
     [ ANSITerminal.cyan; ANSITerminal.Bold ]
     " Main Menu \n";
   ANSITerminal.print_string [ ANSITerminal.cyan ]
-    " >>> [ 1 ]  catch pokemon \n";
+    " >>> [ 1 ] catch\n   pokemon \n";
   ANSITerminal.print_string [ ANSITerminal.cyan ]
-    " >>> [ 2 ]  battle pokemon [not implemented] \n";
+    " >>> [\n   2 ] battle pokemon [not implemented] \n";
   ANSITerminal.print_string [ ANSITerminal.cyan ]
-    " >>> [ 3 ]  battle users [not implemented] \n \n";
+    " >>> [ 3 ] battle users [not implemented] \n\n   \n";
   ANSITerminal.print_string [ ANSITerminal.blue ]
-    " Type in ' 1'  '2' or '3'\n to choose option to play: \n ";
+    " Type in ' 1'\n   '2' or '3'\n to choose option to play: \n ";
   print_string "> ";
   match read_line () with
   | exception End_of_file -> ()
@@ -55,30 +55,39 @@ let rec main_menu_terminal =
 let rec listen_new_user (input : string) =
   ANSITerminal.print_string [ ANSITerminal.cyan ]
     (" \n Hmmm," ^ input
-   ^ " has a good ring to it.   \n\n\
-     \        Welcome to the vast world of Pokemon. \n\
-     \ \n\
-     \        This world is inhabited by creatures big and small, cute \
-      and fierce, \n\
-     \        friendly and dangerous called Pokemon. \n\
-     \ \n\
-     \        My name is Profesor Foster. They call me the Profoster. \n\
-     \ \n\
-     \        My task for you is to explore the regions in this world \
-      and log their information \n\
-     \      into this device I created, the Camldex. \n\
-     \      \n\
+   ^ " has a good ring to it.\n\
+     \   \n\n\
+     \  Welcome to the vast world of Pokemon. \n\
+     \  \n\
+     \  This world\n\
+     \   is inhabited by creatures big and small, cute  and fierce, \n\
+     \ friendly and dangerous called Pokemon. \n\
+     \  \n\
+     \  My name is\n\
+     \   Profesor Foster. They call me the Profoster. \n\
+     \  \n\
+     \  My task for\n\
+     \   you is to explore the regions in this world  and log their\n\
+     \   information \n\
+     \  into this device I created, the Camldex. \n\
+     \  \n\
      \ Do you accept my task of filling this Camldex? (y/y) ");
   print_string "> ";
   match read_line () with
   | _ ->
       ANSITerminal.print_string [ ANSITerminal.blue ]
         " \n\
-        \ You nod your head yes, despite what you really want to say.  \n";
+        \  You nod your\n\
+        \   head yes, despite what you really want to say. \n";
       ANSITerminal.print_string
         [ ANSITerminal.magenta ]
-        " \n You obtained the Camldex!  \n";
+        " \n You obtained\n   the Camldex! \n";
       main_menu_terminal
+
+let enter_game =
+  ANSITerminal.print_string [ ANSITerminal.blue ]
+    " \n\
+    \ You nod your head yes, despite what you really want to say.  \n"
 
 let rec loop () =
   let e = wait_next_event [ Mouse_motion; Key_pressed ] in

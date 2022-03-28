6d8da5a1201e9750fdd18208410459820af5e851
Will Zhang <wz282@cornell.edu>
2022-03-28 12:01:44 -0400

    Main Menu working for GUI
    - moved images into public and have designated folders for different pages
    - trainer.ml changed for mutable state
    - main menu has loading screen, select screen, and new world screen

--- lib/trainer.ml
+++ lib/trainer.ml
@@ -6,3 +6,5 @@ type t = {
 
 let get_pokemon t = t.party
 let init_trainer s n = { name = s; id = n; party = [] }
+let set_trainer_name n st = { st with name = n }
+let get_trainer_name st = st.name
--- lib/trainer.mli
+++ lib/trainer.mli
@@ -7,3 +7,8 @@ val get_pokemon : t -> Pokemon.t list
 
 val init_trainer : string -> int -> t
 (** [init_trainer s n] returns a trainer with name s and id n. *)
+
+val set_trainer_name : string -> t -> t
+(** [set_trainer_name n st] changes the trainer's name to [n] given state [st] *)
+val get_trainer_name : t -> string
+(** [get_trainer_name st] retreives the trainer's name to given state [st] *)
